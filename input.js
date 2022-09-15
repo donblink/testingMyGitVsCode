aa8ad6be83bd99e40fd5d805ca125d320ad8fe86
habib diabagate <hdiabagate@gmail.com>
2022-09-14 19:58:12 -0500

    testing first local commit

---
+++ input.js
@@ -0,0 +1,4 @@
+const prompt = require("prompt-sync")();
+var first = prompt('enter your first name :');
+var lastname = prompt('enter your last name:');
+console.log('your full name is :'+ first +' '+lastname);
